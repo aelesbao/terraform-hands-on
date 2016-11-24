987dd8c487565db679113b957290cc1d759db5ff
Augusto Elesbão <aelesbao@gmail.com>
2016-11-24 11:53:15 +0100

    Increase the count of bastion instances

--- bastion.tf
+++ bastion.tf
@@ -12,9 +12,12 @@ data "aws_ami" "ubuntu" {
 }
 
 resource "aws_instance" "bastion" {
-    ami = "${data.aws_ami.ubuntu.id}"
-    instance_type = "t2.micro"
-    tags {
-      Name = "bastion"
-    }
+  count = 2
+
+  ami = "${data.aws_ami.ubuntu.id}"
+  instance_type = "t2.micro"
+
+  tags {
+    Name = "${format("bastion-%03d", count.index + 1)}"
+  }
 }
