a44608adf62ce3e86d740e77fecf6daaf2d0adde
Augusto Elesbão <aelesbao@gmail.com>
2016-11-24 12:09:10 +0100

    Apply terraform ftm

--- bastion.tf
+++ bastion.tf
@@ -1,20 +1,23 @@
 data "aws_ami" "ubuntu" {
   most_recent = true
+
   filter {
-    name = "name"
+    name   = "name"
     values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
   }
+
   filter {
-    name = "virtualization-type"
+    name   = "virtualization-type"
     values = ["hvm"]
   }
+
   owners = ["099720109477"] # Canonical
 }
 
 resource "aws_instance" "bastion" {
   count = 2
 
-  ami = "${data.aws_ami.ubuntu.id}"
+  ami           = "${data.aws_ami.ubuntu.id}"
   instance_type = "t2.micro"
 
   tags {
